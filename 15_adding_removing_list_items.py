6812f7b8698a087301b4bb83d4c4ab1b162d92ef
Jason Douglas <bones.coding@gmail.com>
2025-05-07 16:10:39 -0400

    new updates

--- 15_adding_removing_list_items.py
+++ 15_adding_removing_list_items.py
@@ -1,3 +1,9 @@
+"""
+List in Python are mutable( meaning 'changeable) ordered( indexed) sequences of items. 
+List are coded by enclosing comma-separated values( items) within square brackets []. 
+Lists can contain items that are of different data types or Python objects.
+"""
+
 # create a list:
 colors = ["red", "orange", "yellow"]
 
@@ -29,6 +35,58 @@ color_tuple = ("brimstone", "chartreuse")
 colors.extend(color_tuple)
 colors.extend(range(3))
 
+print(colors)
+print()
+
+#########################################################
+#                                                       #
+#               Removing List Items                     #
+#                                                       #
+#########################################################
+
+# create a new list:
+elements = ["earth", "wind", "water", "fire"]
+print("Contents of the 'elements' list:")
+print(elements)
+print()
+
+# remove a specified item with the remove() method:
+elements.remove("wind")
+print(elements)
+print()
+
+# lets put it back:
+elements.insert(1, "wind")
+print(elements)
+print()
+
+# make use of the pop() method:
+elements.pop(1)     # if the index was not provided as a parameter, the last item would be popped from the list
+print("The wind is gone again...")
+print(elements)
+print()
+
+# clear a list of its items:
+elements.clear()
+print("Now there are no elements left:")
+print(elements)
+print()
+
+print("Here is our colors list from earlier:")
+print(colors)
+print()
+
+# delete specific items with del():
+del colors[-3]
+del colors[-2]
+del colors[-1]
+print("We have removed all the numbers from the colors list:")
+print(colors)
+print()
+
+print("Back to the rainbow...")
+del colors[7]
+del colors[7]
 print(colors)
 print()
 
---
+++ 16_looping_lists.py
@@ -0,0 +1,16 @@
+"""
+There are few ways of looping through list:
+
+        > Iterate directly over the elements of the list with a 'for' loop.
+
+        > Iterate through the indices of a list with a 'for' loop.
+
+        > Use enumerate() function.
+        >>>>> This functions gives the index and value of each item in the list.
+
+        > Use a 'while' loop.
+        >>>>> A developer must use index management, or a counter, to terminate the loop.
+"""
+
+print()
+
